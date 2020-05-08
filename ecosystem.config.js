0b2bbd6209772cce7ff6d433a676eb9e0f075f97
Lars Karbo <mail@larskarbo.no>
2020-05-08 23:03:10 +0200

    fixed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,13 @@ module.exports = {
   apps : [{
     name: 'habitsstreak',
     script: 'habitsstreak.py',
-    interpreter : ".venv/bin/python",
-  },{
-    name: 'habitsstreakcyri',
-    script: 'habitsstreak-cyri.py',
-    interpreter : ".venv/bin/python",
-  }],
+    interpreter : "pipenv",
+    interpreter_args: "run python"
+  }
+  // ,{
+  //   name: 'habitsstreakcyri',
+  //   script: 'habitsstreak-cyri.py',
+  //   interpreter : ".venv/bin/python",
+  // }
+],
 };
