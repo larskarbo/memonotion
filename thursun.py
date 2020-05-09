d8c3f034be27f45920c82a53118252ea795bef22
Lars Karbo <mail@larskarbo.no>
2020-05-09 15:10:49 +0200

    add telegram message to thursun

--- thursun.py
+++ thursun.py
@@ -7,6 +7,7 @@ from notion.client import NotionClient
 from notion.block import TextBlock, TodoBlock, BulletedListBlock
 from datetime import datetime
 from notionHelpers import findChild
+import requests
 
 # %%
 client = NotionClient(token_v2=os.environ["NOTION_TOKEN"], monitor=True)
@@ -18,8 +19,10 @@ block = client.get_block("https://www.notion.so/larskarbo/Daily-entry-c9e4a17b8b
 today = datetime.today().strftime("%A")
 print(today)
 if today == "Thursday" or today == "Sunday":
+    requests.get("https://api.telegram.org/bot1196576929:AAFCVPBTMcSUlrHAIFBO_Ni7e9em0Nje10U/sendMessage?chat_id=912275377&text=remember thur sun!")
     newchild = block.children.add_new(TodoBlock, title=("done [tor/søn biz](https://www.notion.so/larskarbo/tor-s-n-biz-e7d43e8da88b4cd785f6775fb80ef4d4)?"))
     newchild.move_to(newchild.parent, 'first-child')
 else:
     victim = findChild(block, "tor/søn")
-    victim.remove()
+    if victim:
+        victim.remove()
