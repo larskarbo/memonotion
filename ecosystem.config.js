f8d8f8e525dd9016ca407a63a620a76b08f8f84e
Lars Karbo <mail@larskarbo.no>
2020-05-15 23:15:47 +0200

    hs-cyri eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,16 @@
 module.exports = {
-  apps : [{
-    name: 'habitsstreak',
-    script: 'habitsstreak.py',
-    interpreter : "pipenv",
-    interpreter_args: "run python"
-  }
-  // ,{
-  //   name: 'habitsstreakcyri',
-  //   script: 'habitsstreak-cyri.py',
-  //   interpreter : ".venv/bin/python",
-  // }
-],
+  apps: [
+    {
+      name: "habitsstreak",
+      script: "habitsstreak.py",
+      interpreter: "pipenv",
+      interpreter_args: "run python",
+    },
+    {
+      name: "habitsstreak-cyri",
+      script: "habitsstreak-cyri.py",
+      interpreter: "pipenv",
+      interpreter_args: "run python",
+    },
+  ],
 };
